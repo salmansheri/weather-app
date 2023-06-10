e18badd2012e04c9e860398f68aa2bb443bd3555
salmansheri <sheriffsalman00@gmail.com>
2023-06-10 22:19:51 +0530

    feat: added authentication

--- app/auth/login/index.js
+++ app/auth/login/index.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
-const Login = () => {
+const Register = () => {
   return (
-    <Text>index</Text>
-  )
-}
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "10px",
+      }}
+    >
+      <View
+        style={{
+          width: "80%",
+        }}
+      >
+        
+        <View>
+          <Text style={{ fontSize: "20px" }}>Email</Text>
+          <TextInput
+            style={{
+              marginVertical: "10px",
+              border: "1px solid black",
+              padding: "10px",
+            }}
+            placeholder="Enter Your Email"
+          />
+        </View>
+        <View>
+          <Text style={{ fontSize: "20px" }}>password</Text>
+          <TextInput
+            style={{
+              marginVertical: "10px",
+              border: "1px solid black",
+              padding: "10px",
+            }}
+            placeholder="Enter Your Password"
+          />
+        </View>
+      </View>
+      <Button title="Submit" />
+    </View>
+  );
+};
 
-export default Login
+export default Register;
--- app/auth/register/index.js
+++ app/auth/register/index.js
@@ -1,10 +1,60 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
 const Register = () => {
   return (
-    <Text>Register</Text>
-  )
-}
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "10px",
+      }}
+    >
+      <View
+        style={{
+          width: "80%",
+        }}
+      >
+        <View>
+          <Text style={{ fontSize: "20px" }}>Name</Text>
+          <TextInput
+            style={{
+              marginVertical: "10px",
+              border: "1px solid black",
+              padding: "10px",
+            }}
+            placeholder="Enter Your Name"
+          />
+        </View>
+        <View>
+          <Text style={{ fontSize: "20px" }}>Email</Text>
+          <TextInput
+            style={{
+              marginVertical: "10px",
+              border: "1px solid black",
+              padding: "10px",
+            }}
+            placeholder="Enter Your Email"
+          />
+        </View>
+        <View>
+          <Text style={{ fontSize: "20px" }}>password</Text>
+          <TextInput
+            style={{
+              marginVertical: "10px",
+              border: "1px solid black",
+              padding: "10px",
+            }}
+            placeholder="Enter Your Password"
+          />
+        </View>
+      </View>
+      <Button title="Submit" />
+    </View>
+  );
+};
 
-export default Register
+export default Register;
